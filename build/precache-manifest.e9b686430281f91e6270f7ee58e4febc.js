f57550a43443c76f1e820a6b04cb06f60b1d8c93
James Kan <james0077721@hotmail.com>
2020-08-14 14:27:16 -0700

    Build

---
+++ build/precache-manifest.e9b686430281f91e6270f7ee58e4febc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a164aec2820d1628ec1",
+    "url": "./static/css/main.6445dc0b.chunk.css"
+  },
+  {
+    "revision": "7a164aec2820d1628ec1",
+    "url": "./static/js/main.7a164aec.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "d05d09b72eaf332a58e9",
+    "url": "./static/css/2.26328c19.chunk.css"
+  },
+  {
+    "revision": "d05d09b72eaf332a58e9",
+    "url": "./static/js/2.d05d09b7.chunk.js"
+  },
+  {
+    "revision": "f67ae8f5615e68c545e6d390fa696444",
+    "url": "./static/media/protohack.f67ae8f5.jpg"
+  },
+  {
+    "revision": "785577d1801f8bf30dc196cce8374f57",
+    "url": "./static/media/lumohacks2.785577d1.jpg"
+  },
+  {
+    "revision": "13384616a625ed6986f1c6ec5b0f5719",
+    "url": "./static/media/mechathon.13384616.jpg"
+  },
+  {
+    "revision": "ffe990e33c1ed98b17ebd2ae803e5bcc",
+    "url": "./static/media/lumohacks.ffe990e3.jpg"
+  },
+  {
+    "revision": "b7a40e2d7f1c632f4d0bff2118248432",
+    "url": "./index.html"
+  }
+];
